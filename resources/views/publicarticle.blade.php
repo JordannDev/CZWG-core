3145f85fadab4b2aadc050409ba71daff199ba0e
WinnipegFIR <virtualwinnipegfir@gmail.com>
2020-06-02 15:16:50 -0700

    Public Article Layout change

--- resources/views/publicarticle.blade.php
+++ resources/views/publicarticle.blade.php
@@ -4,16 +4,19 @@
 @section('description', $article->summary)
 
 @section('content')
-    <div class="card card-image blue rounded-0" style="background-image: url({{$article->image}}); background-size: cover; background-position: center;">
-        <div class="text-white text-left py-1 px-4 rgba-black-light">
+    <div class="card card-image blue rounded-0">
+
+    </div>
+        <div class="text-white text-left py-1 px-4" style="background-color:#013162">
             <div class="container">
                 <div class="py-5">
-                    <h1 class="h1" style="font-size: 3em;">{{$article->title}}</h1>
+                    <h1 align="center" class="h1" style="font-size: 3em;">{{$article->title}}</h1>
                 </div>
             </div>
         </div>
     </div>
     <div class="container py-4">
+      <img src="{{$article->image}}" alt="" title="" width="100%" height="40%">
         <h4 class="blue-text">{{$article->summary}}</h4>
         <div class="d-flex flex-row align-items-center">
             <i class="far fa-clock"></i>&nbsp;&nbsp;<span @if($article->edited) title="Last edited {{$article->edited_pretty()}}" @endif>Published {{$article->published_pretty()}}</span>&nbsp;&nbsp;•&nbsp;&nbsp;<i class="far fa-user-circle"></i>&nbsp;&nbsp;{{$article->author_pretty()}}
